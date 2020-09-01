e5543d06d0d8259cd3ffd56c36c48e4d86a13a05
Alexangelj <alexangel.j@gmail.com>
2020-09-01 11:16:59 -0700

    Added exercise, redeem, and close to primitive lib file.

--- src/lib/primitive.ts
+++ src/lib/primitive.ts
@@ -26,11 +26,16 @@ const getTraderAddress = (chainId) => {
     return address;
 };
 
-const mint = async (signer, quantity, optionAddress) => {
+const getTrader = async (signer) => {
     const chain = await signer.getChainId();
     const traderAddress = getTraderAddress(chain);
     const trader = new ethers.Contract(traderAddress, Trader.abi, signer);
+    return { traderAddress, trader };
+};
+
+const mint = async (signer, quantity, optionAddress) => {
     const account = await signer.getAddress();
+    const { traderAddress, trader } = await getTrader(signer);
     const option = new ethers.Contract(optionAddress, Option.abi, signer);
     const underlyingAddress = await option.getUnderlyingTokenAddress();
     const mintQuantity = parseEther(quantity).toString();
@@ -44,4 +49,54 @@ const mint = async (signer, quantity, optionAddress) => {
     }
 };
 
-export { mint };
+const exercise = async (signer, quantity, optionAddress) => {
+    const account = await signer.getAddress();
+    const { traderAddress, trader } = await getTrader(signer);
+    const option = new ethers.Contract(optionAddress, Option.abi, signer);
+    const strikeAddress = await option.getStrikeTokenAddress();
+    const exerciseQuantity = parseEther(quantity).toString();
+
+    await checkAllowance(signer, strikeAddress, traderAddress);
+    await checkAllowance(signer, optionAddress, traderAddress);
+
+    try {
+        await trader.safeExercise(optionAddress, exerciseQuantity, account);
+    } catch (err) {
+        console.log("Error on exercising: ", err);
+    }
+};
+
+const redeem = async (signer, quantity, optionAddress) => {
+    const account = await signer.getAddress();
+    const { traderAddress, trader } = await getTrader(signer);
+    const option = new ethers.Contract(optionAddress, Option.abi, signer);
+    const redeemAddress = await option.redeemToken();
+    const redeemQuantity = parseEther(quantity).toString();
+
+    await checkAllowance(signer, redeemAddress, traderAddress);
+
+    try {
+        await trader.safeRedeem(optionAddress, redeemQuantity, account);
+    } catch (err) {
+        console.log("Error on redeeming: ", err);
+    }
+};
+
+const close = async (signer, quantity, optionAddress) => {
+    const account = await signer.getAddress();
+    const { traderAddress, trader } = await getTrader(signer);
+    const option = new ethers.Contract(optionAddress, Option.abi, signer);
+    const redeemAddress = await option.redeemToken();
+    const closeQuantity = parseEther(quantity).toString();
+
+    await checkAllowance(signer, redeemAddress, traderAddress);
+    await checkAllowance(signer, optionAddress, traderAddress);
+
+    try {
+        await trader.safeClose(optionAddress, closeQuantity, account);
+    } catch (err) {
+        console.log("Error on closing: ", err);
+    }
+};
+
+export { mint, exercise, redeem, close };
