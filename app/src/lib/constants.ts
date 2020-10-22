eae49329df90f0f2d2788bc64c8ed95eee16abbf
Zachary Thielemann <zachdt@gmail.com>
2020-10-22 11:59:01 -0700

    updated SDK

---
+++ app/src/lib/constants.ts
@@ -0,0 +1,20 @@
+export enum Direction {
+  LONG,
+  SHORT,
+  NEUTRAL,
+}
+
+export enum Operation {
+  MINT,
+  EXERCISE,
+  REDEEM,
+  CLOSE,
+  UNWIND,
+}
+
+export const STABLECOIN_ADDRESS = ''
+
+export const OPTION_FACTORY_ADDRESS = ''
+
+export const OPTION_SALT: string =
+  '0x56f3a99c8e36689645460020839ea1340cbbb2e507b7effe3f180a89db85dd87'
