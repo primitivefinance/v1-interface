453d4264a00d3b179b0ecb77bf2880377397a9de
Alexangelj <alexangel.j@gmail.com>
2020-10-22 10:20:19 -0700

    Add option salt to constants.

--- sdk/src/constants.ts
+++ sdk/src/constants.ts
@@ -13,3 +13,8 @@ export enum Operation {
 }
 
 export const STABLECOIN_ADDRESS = ''
+
+export const OPTION_FACTORY_ADDRESS = ''
+
+export const OPTION_SALT: string =
+  '0x56f3a99c8e36689645460020839ea1340cbbb2e507b7effe3f180a89db85dd87'
