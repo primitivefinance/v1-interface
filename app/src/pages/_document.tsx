24e73d400aed0aefcfa7f2720c53d66f57abae0f
Zachary Thielemann <zachdt@gmail.com>
2020-11-15 22:27:09 -0800

    repaired material-ui styles using document enhancement

--- app/src/pages/_document.tsx
+++ app/src/pages/_document.tsx
@@ -1,33 +1,41 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheets } from '@material-ui/styles'
+// THIS FILE IS REQUIRED FOR MATERIAL-UI STYLES TO LOAD IN NEXT.JS
+//  reference: https://github.com/mui-org/material-ui/blob/master/examples/nextjs/pages/_document.js
+//
 import { ServerStyleSheet } from 'styled-components'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
+    const styledComponentsSheet = new ServerStyleSheet()
+    const materialSheets = new ServerStyleSheets()
     const originalRenderPage = ctx.renderPage
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
+            styledComponentsSheet.collectStyles(
+              materialSheets.collect(<App {...props} />)
+            ),
         })
-
       const initialProps = await Document.getInitialProps(ctx)
       return {
         ...initialProps,
         styles: (
-          <>
+          <React.Fragment>
             {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
+            {materialSheets.getStyleElement()}
+            {styledComponentsSheet.getStyleElement()}
+          </React.Fragment>
         ),
       }
     } finally {
-      sheet.seal()
+      styledComponentsSheet.seal()
     }
   }
+
   render() {
     return (
       <Html>
