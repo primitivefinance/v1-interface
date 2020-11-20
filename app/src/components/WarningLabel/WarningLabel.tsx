b92ca29bf862c22a58897da9f89382a7ca75bbb4
Alexangelj <alexangel.j@gmail.com>
2020-11-19 19:43:44 -0800

    Partial fix for #205. Adds ui guard caps for mainnet. Needs actual price info.

---
+++ app/src/components/WarningLabel/WarningLabel.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const WarningLabel: React.FC = ({ children }) => (
+  <StyledWarningLabel>{children}</StyledWarningLabel>
+)
+
+const StyledWarningLabel = styled.div`
+  color: ${(props) => props.theme.color.red[500]};
+  display: flex;
+  font-size: 14px;
+  justify-content: center;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  opacity: 0.66;
+  width: 100%;
+`
+
+export default WarningLabel
