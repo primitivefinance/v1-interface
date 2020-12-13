52e61a13731e0a3ecb67fcbaeaacdd6d5e0ae1c2
Zachary Thielemann <zachdt@gmail.com>
2020-12-12 23:09:48 -0800

    fix: minor formatting fix, 502 error page

---
+++ app/src/pages/_error.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import styled from 'styled-components'
+import Spacer from '@/components/Spacer'
+import Link from 'next/link'
+
+const Error: React.FC = () => {
+  const router = useRouter()
+
+  const handleClick = () => {
+    router.push('/markets')
+  }
+  return (
+    <>
+      <Spacer />
+      <Spacer />
+      <StyledTitle>500 Error</StyledTitle>
+    </>
+  )
+}
+
+const StyledTitle = styled.div`
+  font-size: 36px;
+  font-weight: bold;
+  color: ${(props) => props.theme.color.red};
+`
+const StyledSub = styled.a`
+  font-size: 20px;
+  font-weight: bold;
+  color: ${(props) => props.theme.color.grey[400]};
+`
+
+export default Error
