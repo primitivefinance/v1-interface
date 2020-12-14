07a45eb6f6c633c464e0b34700bbc2ad0ee89941
Zachary Thielemann <zachdt@gmail.com>
2020-12-13 21:32:22 -0800

    fix: header loading

--- app/src/components/Market/MarketHeader/MarketHeader.tsx
+++ app/src/components/Market/MarketHeader/MarketHeader.tsx
@@ -168,7 +168,7 @@ const MarketHeader: React.FC<MarketHeaderProps> = ({ marketId, isCall }) => {
                 <>
                   <div style={{ minHeight: '.4em' }} />
                   <Loader size="sm" />
-                  <div style={{ minHeight: '.4em' }} />
+                  <div style={{ minHeight: '.55em' }} />
                 </>
               )}
             </StyledPrice>
