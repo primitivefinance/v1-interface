1bfe2c79c158da35b3f243ebdcd11e44f8656eb9
Zachary Thielemann <zachdt@gmail.com>
2020-12-22 15:50:44 -0800

    fix(addLiquidity): propagated validation changes

--- app/src/components/Market/OrderCard/components/AddLiquidity/AddLiquidity.tsx
+++ app/src/components/Market/OrderCard/components/AddLiquidity/AddLiquidity.tsx
@@ -105,6 +105,9 @@ const AddLiquidity: React.FC = () => {
   const handleOptionInput = useCallback(
     (value: string) => {
       onOptionInput(value)
+      if (value === '0.') {
+        value = '0'
+      }
       if (tab === 1) {
         onUnderInput(
           formatEther(
