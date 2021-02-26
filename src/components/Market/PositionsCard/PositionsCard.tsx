de5481fd392ede2929d63a221341a4bad69c9f4d
Zachary Thielemann <zachdt@gmail.com>
2021-02-26 14:45:36 -0800

    fix(position): short to long

--- src/components/Market/PositionsCard/PositionsCard.tsx
+++ src/components/Market/PositionsCard/PositionsCard.tsx
@@ -32,7 +32,7 @@ const Position: React.FC<TokenProps> = ({ option }) => {
   const entity = option.attributes.entity
 
   const handleClick = () => {
-    updateItem(option.attributes, Operation.SHORT)
+    updateItem(option.attributes, Operation.LONG)
   }
 
   const getScaledBalances = useCallback(() => {
